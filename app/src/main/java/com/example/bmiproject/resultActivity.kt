7ca4acbdac57758ab1a99f8980448d79bcb4ea1d
whtjtjddn <54929329+whtjtjddn@users.noreply.github.com>
2022-02-08 13:35:41 +0900

    Double 소수점 2자리까지만 나오도록 수정

---
+++ app/src/main/java/com/example/bmiproject/resultActivity.kt
@@ -0,0 +1,34 @@
+package com.example.bmiproject
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import kotlin.math.pow
+import kotlin.math.round
+
+class ResultActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+
+        val height = intent.getIntExtra("height", 0)
+        val weight = intent.getIntExtra("weight", 0)
+
+        val bmi = round(weight / (height / 100.0).pow(2.0) * 100) / 100
+
+        val resultText = when{
+            bmi >= 35.0 -> "고도 비만"
+            bmi >= 30.0 -> "중정도 비만"
+            bmi >= 25.0 -> "경도 비만"
+            bmi >= 23.0 -> "과체중"
+            bmi >= 18.5 -> "정상 체중"
+            else -> "저체중"
+        }
+        val resultValueTextView = findViewById<TextView>(R.id.bmiResultTextView)
+        val resultStringTextView = findViewById<TextView>(R.id.resultTextView)
+
+        resultValueTextView.text = bmi.toString()
+        resultStringTextView.text = resultText
+    }
+}
